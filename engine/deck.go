f5dc2f6a55b7461443e987aa28d07cda4c50b74e
Edward Nolan <nolanee@umich.edu>
2020-05-13 21:24:54 -0400

    finished Deal and tests, added new file to put deck functions in

---
+++ engine/deck.go
@@ -0,0 +1,17 @@
+package engine
+
+import "errors"
+
+// Deal takes a slice of Cards and returns a slice of numDealt Cards from it.
+// If the slice of cards is not big enough, it returns an empty slice and
+// an error. Deck is reduced in size by numDealt.
+
+func Deal (numDealt uint, deck *[]Card) (hand []Card, err error) {
+	d := *deck
+	if len(d) < int(numDealt) {
+		return hand, errors.New("Deal: not enough cards in deck")
+	}
+	hand = d[:numDealt]
+	*deck = d[numDealt:]
+	return
+}
