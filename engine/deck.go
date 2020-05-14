9fa8ba70aa56669fe52f52bc75efc0fd4cda50b6
Edward Nolan <nolanee@umich.edu>
2020-05-13 21:27:54 -0400

    edited comments

--- engine/deck.go
+++ engine/deck.go
@@ -2,9 +2,9 @@ package engine
 
 import "errors"
 
-// Deal takes a slice of Cards and returns a slice of numDealt Cards from it.
-// If the slice of cards is not big enough, it returns an empty slice and
-// an error. Deck is reduced in size by numDealt.
+// Deal takes a pointer to deck (slice) of Cards and returns a slice of numDealt 
+// Cards from it. If the slice of cards is not big enough, it returns an empty 
+// slice and an error. Deck is reduced in size by numDealt.
 
 func Deal (numDealt uint, deck *[]Card) (hand []Card, err error) {
 	d := *deck
