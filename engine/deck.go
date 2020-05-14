5a9da9038cdbdb4230b1895dca466d9ed8b325b4
Edward Nolan <nolanee@umich.edu>
2020-05-13 22:51:19 -0400

    updated name of deal to reflect its privacy

--- engine/deck.go
+++ engine/deck.go
@@ -6,10 +6,7 @@ import (
 	"time"
 )
 
-// Deal takes a pointer to deck (slice) of Cards and returns a slice of numDealt
-// Cards from it. If the slice of cards is not big enough, it returns an empty
-// slice and an error. Deck is reduced in size by numDealt.
-func Deal(numDealt uint, deck *[]Card) (hand []Card, err error) {
+func deal(numDealt uint, deck *[]Card) (hand []Card, err error) {
 	d := *deck
 	if len(d) < int(numDealt) {
 		return hand, errors.New("Deal: not enough cards in deck")
